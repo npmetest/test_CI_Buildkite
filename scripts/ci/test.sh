74bf0253ba9fe3280bbc6a5d460040718db8e720
amol-deole <amol.deole@zibtek.com>
2018-03-12 23:04:00 +0530

    Added domain name instead of IP registry

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -4,6 +4,6 @@ set -eu
 
 export NPM_TOKEN="977a5fe4-f902-4bd6-97a1-3db4c005272b"
 
-printf "@zibtek:registry=http://192.168.249.152:8080\n//192.168.249.152:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+printf "@zibtek:registry=http://npme.zibtek.us:8080\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 npm install
