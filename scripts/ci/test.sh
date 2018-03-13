f9cd2380ffbe297fedc06ddb1a12ce9039a36f34
amol-deole <amol.deole@zibtek.com>
2018-03-14 01:12:24 +0530

    Made some changes in the script file

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -6,6 +6,6 @@ export NPM_TOKEN="5014aab2cfe61ee8256c9bfdbce8e64fdaca63c4"
 
 echo $NPM_TOKEN
 
-echo "@zibtek:registry=http://npme.zibtek.us:8080\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+printf "@zibtek:registry=http://npme.zibtek.us:8080/\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 npm install
