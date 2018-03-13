749aaf9211e78b68f915f1af7f33d13e2f66cd4c
amol-deole <amol.deole@zibtek.com>
2018-03-14 02:03:15 +0530

    Gave the root permission

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -5,8 +5,8 @@ export NPM_TOKEN="3b4325b6-02ab-4929-8128-20ae0170c0ac"
 
 echo $NPM_TOKEN
 
-printf "@zibtek:registry=http://npme.zibtek.us:8080/\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+printf "\n@zibtek:registry=http://npme.zibtek.us:8080/\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 cat ~/.npmrc
 
-npm install --verbose
+sudo npm install --verbose
