07d97199bcbc1a0f1d25cc1de3f6edf50693328b
amol-deole <amol.deole@zibtek.com>
2018-03-13 21:03:18 +0530

    Added the envrionment variable in pipeline

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -6,6 +6,6 @@ export NPM_TOKEN="5014aab2cfe61ee8256c9bfdbce8e64fdaca63c4"
 
 echo $NPM_TOKEN
 
-printf "@zibtek:registry=http://npme.zibtek.us:8080\n//npme.zibtek.us:8080/:_authToken=5014aab2cfe61ee8256c9bfdbce8e64fdaca63c4" >> ~/.npmrc
+printf "@zibtek:registry=http://npme.zibtek.us:8080\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 npm install
