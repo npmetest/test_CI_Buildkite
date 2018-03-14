920db66bcc03ed1a112590cd5498b7694c135307
amol-deole <amol.deole@zibtek.com>
2018-03-15 02:18:35 +0530

    Changed npmE token

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 
-export NPM_TOKEN="3b4325b6-02ab-4929-8128-20ae0170c0ac"
+#export NPM_TOKEN="3b4325b6-02ab-4929-8128-20ae0170c0ac"
 
 echo $NPM_TOKEN
 
 printf "\n@zibtek:registry=http://npme.zibtek.us:8080/\n//npme.zibtek.us:8080/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 cat ~/.npmrc
+
+npm cache clear --force
 
 npm install --verbose
